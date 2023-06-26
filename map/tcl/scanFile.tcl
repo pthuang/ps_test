67afae52869505272a107e60d32d2c87dce40ec4
weitao_zhao <weitao_zhao@sunwave.com.cn>
2023-06-26 10:38:03 +0800

    更新扫描脚本，解决当文件夹不存在时扫描报错，过程终止的BUG

--- map/tcl/scanFile.tcl
+++ map/tcl/scanFile.tcl
@@ -1,65 +1,116 @@
-# process 
+###############################################################
+# process decalration 
+###############################################################
 source findFiles.tcl
 
 
-#######################################
+###############################################################
 # scan source files 
-#######################################
-set srcFileName "./listSrc.txt"
-set srcPath $hdlDir
-set status [catch {set fid [open $srcFileName w+]} msg]
-if {$status} { 
-	puts $msg
+###############################################################
+cd $fpgaXprDir
+if {[file exist src]} {
+	cd $tclDir
+	# scan 
+	set srcFileName "./listSrc.txt"
+	set srcPath $hdlDir
+	set status [catch {set fid [open $srcFileName w+]} msg]
+	if {$status} { 
+		puts $msg
+	} 
+	findFiles $srcPath $fid ".v" ".vh" ".vhd" ".sv"
+	close $fid
+} else {
+	puts "Directory is not exist and will create it!"
+	file mkdir src 
+	cd $tclDir
 }
-findFiles $srcPath $fid ".v" ".vh" ".vhd" ".sv"
-close $fid
 
 
-#######################################
+###############################################################
 # scan simulation files 
-#######################################
-set simFileName "./listSim.txt"
-set simPath $simDir
-set status [catch {set fid [open $simFileName w+]} msg]
-if {$status} { 
-	puts $msg
+###############################################################
+cd $fpgaXprDir
+if {[file exist sim]} {
+	cd $tclDir
+	# scan 
+	set simFileName "./listSim.txt"
+	set simPath $simDir
+	set status [catch {set fid [open $simFileName w+]} msg]
+	if {$status} { 
+		puts $msg
+	}
+	findFiles $simPath $fid ".v" ".vh" ".vhd" ".sv"
+	close $fid
+} else {
+	puts "Directory is not exist and will create it!"
+	file mkdir sim 
+	cd $tclDir
 }
-findFiles $simPath $fid ".v" ".vh" ".vhd" ".sv"
-close $fid
 
-#######################################
+
+###############################################################
 # scan block design files 
-#######################################
-set bdFileName "./listBd.txt"
-set bdPath $bdDir
-set status [catch {set fid [open $bdFileName w+]} msg]
-if {$status} { 
-	puts $msg
+###############################################################
+cd $srcDir
+if {[file exist bd]} {
+	cd $tclDir
+	# scan 
+	set bdFileName "./listBd.txt"
+	set bdPath $bdDir
+	set status [catch {set fid [open $bdFileName w+]} msg]
+	if {$status} { 
+		puts $msg
+	}
+	findFiles $bdPath $fid ".bd"
+	close $fid
+} else {
+	puts "Directory is not exist and will create it!"
+	file mkdir bd 
+	cd $tclDir
 }
-findFiles $bdPath $fid ".bd"
-close $fid
 
-#######################################
+
+###############################################################
 # scan ip files 
-#######################################
-set ipFileName "./listIp.txt"
-set ipPath $ipDir
-set status [catch {set fid [open $ipFileName w+]} msg]
-if {$status} { 
-	puts $msg
+###############################################################
+cd $srcDir
+if {[file exist ip]} {
+	cd $tclDir
+	# scan 
+	set ipFileName "./listIp.txt"
+	set ipPath $ipDir
+	set status [catch {set fid [open $ipFileName w+]} msg]
+	if {$status} { 
+		puts $msg
+	} 
+	findFiles $ipPath $fid ".xci"
+	close $fid
+} else {
+	puts "Directory is not exist and will create it!"
+	file mkdir ip 
+	cd $tclDir
 }
-findFiles $ipPath $fid ".xci"
-close $fid
 
 
-#######################################
+###############################################################
 # scan constrains files 
-#######################################
-set constrFileName "./listConstr.txt"
-set constrPath $constrDir
-set status [catch {set fid [open $constrFileName w+]} msg]
-if {$status} { 
-	puts $msg
+###############################################################
+cd $srcDir
+if {[file exist constrs]} {
+	cd $tclDir
+	# scan 
+	set constrFileName "./listConstr.txt"
+	set constrPath $constrDir
+	set status [catch {set fid [open $constrFileName w+]} msg]
+	if {$status} { 
+		puts $msg
+	}
+	findFiles $constrPath $fid ".xdc" ".ucf" 
+	close $fid
+} else {
+	puts "Directory is not exist and will create it!"
+	file mkdir constrs 
+	cd $tclDir
 }
-findFiles $constrPath $fid ".xdc" ".ucf" 
-close $fid
+
+
